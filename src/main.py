d2628bacce3ab9b1ef5902979daa5eec7b46fac6
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-10-30 23:08:46 +0530

    Drop csv and use my Telegram Bot to inform me about the message
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- src/main.py
+++ src/main.py
@@ -13,6 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import os
 import sys
 
 try:
@@ -23,24 +24,31 @@ except ImportError:
 app = Flask(__name__)
 
 
+def bot_message(data):
+    # Get the bot API key from the environment variable
+    BOT_API = os.getenv("BOT_API")
+    # Get the chat ID from the environment variable
+    CHAT_ID = os.getenv("CHAT_ID")
+    email = data.get("email")
+    subject = data.get("subject")
+    message = data.get("message")
+    TEXT_MESSAGE = f"Email:```{email}```\nSubject:```{subject}```\nMessage:```{message}```"
+    url = f"https://api.telegram.org/bot{BOT_API}/sendMessage"
+    # use curl to send the message to the bot API and the chat ID of telegram group where the bot is joined
+    os.system(
+        f"curl -s -X POST '{url}' -d chat_id={CHAT_ID} -d text='{TEXT_MESSAGE}' -d parse_mode='Markdown'")
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-def write_to_file(data):
-    with open('submits.csv', mode='a') as database:
-        email = data.get("email")
-        subject = data.get("subject")
-        message = data.get("message")
-        database.write(f'\n{email},{subject},{message}')
 
 
 @app.route('/send_message', methods=['POST'])
 def send_message():
     if request.method == 'POST':
         data = request.form.to_dict()
-        write_to_file(data)
+        bot_message(data)
         return render_template('/thankyou.html')
     else:
         return render_template('/error.html')
