622c16626a18f1f7b1e826398d413ad300e935f6
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-03-05 13:04:59 +0530

    Import initial portfolio template
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

---
+++ src/main.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2022 Hemant Sachdeva <hemant.evolver@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+import sys
+
+try:
+    from flask import Flask, render_template, request
+except ImportError:
+    sys.exit("[!] Flask module not found. Install it by 'pip3 install flask'")
+
+application = Flask(__name__)
+
+
+@application.route('/')
+def index():
+    return render_template('index.html')
